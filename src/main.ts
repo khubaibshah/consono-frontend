45643aef84acc2bd7c31c4314c1f7e3f952f92dd
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-07-29 20:02:35 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+//prime config
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
+import StyleClass from 'primevue/styleclass'
+
+import Ripple from 'primevue/ripple'
+//vue imports
+import App from './App.vue'
+import router from './router'
+
+//components
+import Button from 'primevue/button'
+import Card from 'primevue/card'
+
+const app = createApp(App)
+
+app.directive('styleclass', StyleClass)
+app.directive('ripple', Ripple)
+
+app.use(
+  PrimeVue,
+  {
+    theme: {
+      preset: Aura
+    }
+  },
+  { ripple: true, style: true }
+)
+
+app.component('prime-button', Button)
+app.component('prime-card', Card)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <prime-button label="Check" icon="pi pi-user" />
+    <prime-card>
+      <template #title>Simple Card</template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+          repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa
+          ratione quam perferendis esse, cupiditate neque quas!
+        </p>
+      </template>
+    </prime-card>
+  </main>
+</template>
