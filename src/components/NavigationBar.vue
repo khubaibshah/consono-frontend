2c5d90481178705fb3fa686d7ba11d31a527f197
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-07-30 21:28:36 +0100

    - removing unnesseccary code

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
@@ -8,7 +8,6 @@ import { RouterLink, RouterView } from 'vue-router'
       class="surface-overlay py-3 px-6 flex align-items-center justify-content-between relative lg:static"
       style="min-height: 80px"
     >
-      <!-- <img src="images/blocks/logos/bastion-700.svg" alt="Image" height="40" class="mr-0 lg:mr-6" /> -->
       <a
         v-ripple
         class="cursor-pointer block lg:hidden text-700 p-ripple"
@@ -152,11 +151,6 @@ import { RouterLink, RouterView } from 'vue-router'
               v-ripple
               class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center hover:surface-100 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple"
             >
-              <!-- <img
-                src="images/blocks/avatars/circle/avatar-f-1.png"
-                class="mr-3 lg:mr-0"
-                style="width: 32px; height: 32px"
-              /> -->
               <div class="block lg:hidden">
                 <div class="text-900 font-medium">Josephine Lillard</div>
                 <span class="text-600 font-medium text-sm">Marketing Specialist</span>
@@ -166,33 +160,6 @@ import { RouterLink, RouterView } from 'vue-router'
         </ul>
       </div>
     </div>
-    <!-- <ul
-      class="list-none py-3 px-5 m-0 surface-section flex align-items-center font-medium overflow-x-auto border-top-1 border-bottom-1 surface-border"
-    >
-      <li class="pr-3">
-        <a class="cursor-pointer">
-          <i class="pi pi-home text-blue-500"></i>
-        </a>
-      </li>
-      <li class="px-2">
-        <i class="pi pi-angle-right text-500"></i>
-      </li>
-      <li class="px-2">
-        <a class="cursor-pointer text-blue-500 white-space-nowrap">Level 3</a>
-      </li>
-      <li class="px-2">
-        <i class="pi pi-angle-right text-500"></i>
-      </li>
-      <li class="px-2">
-        <a class="cursor-pointer text-blue-500 white-space-nowrap">Level 2</a>
-      </li>
-      <li class="px-2">
-        <i class="pi pi-angle-right text-500"></i>
-      </li>
-      <li class="px-2">
-        <a class="cursor-pointer text-900 white-space-nowrap">Level 1</a>
-      </li>
-    </ul> -->
     <div class="p-5 flex flex-column flex-auto">
       <div class="surface-border border-round surface-section flex-auto">
         <RouterView />
